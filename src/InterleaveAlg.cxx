942b22678a59b7a9cbeba7f7f60b55f027db7d00
burnett <>
2005-12-17 19:13:56 +0000

    almost ready, lacking  mostly code to make a full TTree copy from the background to the local tuple.

--- src/InterleaveAlg.cxx
+++ src/InterleaveAlg.cxx
@@ -2,12 +2,10 @@
 
 @brief declaration and definition of the class InterleaveAlg
 
-$Header$
+$Header: /nfs/slac/g/glast/ground/cvs/Interleave/src/InterleaveAlg.cxx,v 1.1.1.1 2005/12/14 20:38:27 burnett Exp $
 
 */
 
-
-// Include files
 
 #include "InterleaveAlg.h"
 
@@ -17,7 +15,6 @@ $Header$
 #include "GaudiKernel/SmartDataPtr.h"
 #include "GaudiKernel/SmartRefVector.h"
 
-
 // TDS class declarations: input data, and McParticle tree
 #include "Event/MonteCarlo/McParticle.h"
 #include "Event/TopLevel/Event.h"
@@ -26,8 +23,11 @@ $Header$
 
 // to write a Tree with entryosure info
 #include "ntupleWriterSvc/INTupleWriterSvc.h"
-
-//#include "astro/GPS.h"
+  
+// ROOT includes
+#include "TTree.h"
+#include "TFile.h"
+#include "TLeaf.h"
 
 
 #include <cassert>
@@ -36,24 +36,25 @@ $Header$
 #include <fstream>
 #include <string>
 
-
-
 //------------------------------------------------------------------------
 
 static const AlgFactory<InterleaveAlg>  Factory;
 const IAlgFactory& InterleaveAlgFactory = Factory;
 
-double InterleaveAlg::s_triggerRate=10e3;
+double InterleaveAlg::s_triggerRate=1;
 double InterleaveAlg::s_downlinkRate;
 
 //------------------------------------------------------------------------
 //! ctor
 InterleaveAlg::InterleaveAlg(const std::string& name, ISvcLocator* pSvcLocator)
-:Algorithm(name, pSvcLocator) 
+:Algorithm(name, pSvcLocator)
+, m_count(0), m_downlink(0), m_event(0), m_meritTuple(0)
 {
     // declare properties with setProperties calls
     declareProperty("TriggerRate",  s_triggerRate=10000.);
     declareProperty("DownLinkRate", s_downlinkRate=400.);
+    declareProperty("RootFile",     m_rootFile="");
+    declareProperty("TreeName",     m_treeName="MeritTuple");
 
 }
 //------------------------------------------------------------------------
@@ -68,12 +69,30 @@ StatusCode InterleaveAlg::initialize(){
 
     // get a pointer to RootTupleSvc
     sc = service("RootTupleSvc", m_rootTupleSvc);
-
     if( sc.isFailure() ) {
-        log << MSG::ERROR << "userAlg failed to get the RootTupleSvc" << endreq;
+        log << MSG::ERROR << "failed to get the RootTupleSvc" << endreq;
         return sc;
     }
+    TFile* m_tfile = new TFile(m_rootFile.value().c_str(), "readonly");
+    if( 0==m_tfile || !m_tfile->IsOpen()){
+        log << MSG::ERROR << "Could not open the root file " << m_rootFile << endreq;
+        return StatusCode::FAILURE;
+    }
+    m_backgroundTuple =  static_cast<TTree*>(m_tfile->Get(m_treeName.value().c_str()));
+    if( 0==m_backgroundTuple) {
+        log << MSG::ERROR << "Did not find the tree in the root file " << m_rootFile << endreq;
+        return StatusCode::FAILURE;
+    }
 
+    if( m_meritTuple==0){
+        void * ptr= 0;
+        m_rootTupleSvc->getItem(m_treeName.value().c_str(),"", ptr);
+        if( ptr==0){
+            log << MSG::ERROR << "Could not find the merit tuple" << endreq;
+            return StatusCode::FAILURE;
+        }
+        m_meritTuple = static_cast<TTree*>(ptr);
+    }
     return sc;
 }
 
@@ -85,18 +104,14 @@ StatusCode InterleaveAlg::execute()
     StatusCode  sc = StatusCode::SUCCESS;
     MsgStream   log( msgSvc(), name() );
 
-    SmartDataPtr<Event::EventHeader>   header(eventSvc(),    EventModel::EventHeader);
-    SmartDataPtr<Event::MCEvent>     mcheader(eventSvc(),    EventModel::MC::Event);
-    SmartDataPtr<Event::McParticleCol> particles(eventSvc(), EventModel::MC::McParticleCol);
+    // check that the TDS has an appropriate pseudo-background 
 
-    double t = header->time();
-    log << MSG::DEBUG << "Event time: " << t << endreq;;
+    SmartDataPtr<Event::McParticleCol> particles(eventSvc(), EventModel::MC::McParticleCol);
 
     if (0==particles) {
         log << MSG::ERROR << "No MC particles!" << endreq;
         return StatusCode::FAILURE;
     }   
-    Event::McParticleCol::const_iterator piter;
         
     const Event::McParticle& primary = **particles->begin();
     double ke = primary.initialFourMomentum().e()-primary.initialFourMomentum().m();
@@ -106,6 +121,24 @@ StatusCode InterleaveAlg::execute()
 
     // Found a sampled_background particle
 
+    ++m_count;
+    ++m_downlink;
+
+    if( ++m_event > m_backgroundTuple->GetEntries() ) {
+        log << MSG::WARNING << "Ran out of event to mix: restarting" << endreq;
+        m_event = 0;
+    }
+    m_backgroundTuple->GetEvent(m_event);
+    
+    // revise stuff in the background tuple to agree with the current event heaader
+    copyEventInfo(m_backgroundTuple);
+
+
+    // now copy every value from the background tree to the local merit tree
+    copyTreeData(m_backgroundTuple, m_meritTuple);
+
+
+    m_rootTupleSvc->storeRowFlag( m_treeName.value(), true);
 
     return StatusCode::SUCCESS;
 }
@@ -119,6 +152,43 @@ StatusCode InterleaveAlg::finalize(){
     done=true;
     MsgStream log(msgSvc(), name());
 
+    log << MSG::INFO << "Processed "<< m_count << " sampled background events, of which "<< m_downlink<< " were passed." << endreq; 
     return sc;
 }
 
+//------------------------------------------------------------------------
+namespace {
+    //! Utility to set a scalar value in a ROOT tree. 
+    //! Assume that the type is known!
+template <typename  Type> 
+void setLeafValue(TLeaf* leaf, Type newvalue)
+{
+    Type& rval = *static_cast<Type*>(leaf->GetValuePointer());
+    rval = newvalue;
+}
+
+}
+//------------------------------------------------------------------------
+void InterleaveAlg::copyEventInfo(TTree * tree)
+{
+
+    SmartDataPtr<Event::EventHeader>   header(eventSvc(),    EventModel::EventHeader);
+// these types *must* correspond with those in EvtValsTool
+    float EvtRun           = header->run();
+    float EvtEventId       = header->event();
+    double EvtElapsedTime  = header->time();
+    float EvtLiveTime      = header->livetime();
+
+    setLeafValue(tree->GetLeaf("EvtRun"),        EvtRun);
+    setLeafValue(tree->GetLeaf("EvtEventId"),    EvtEventId);
+    setLeafValue(tree->GetLeaf("EvtElapsedTime"),EvtElapsedTime);
+    setLeafValue(tree->GetLeaf("EvtLiveTime"),   EvtLiveTime);
+
+}
+//------------------------------------------------------------------------
+void InterleaveAlg::copyTreeData(TTree * in, TTree* out)
+{
+    // TODO: fill this
+    copyEventInfo(out); // the full copy would do this.
+
+}
--- src/InterleaveAlg.h
+++ src/InterleaveAlg.h
@@ -2,7 +2,7 @@
 
 @brief declaration  of the class InterleaveAlg
 
-$Header$
+$Header: /nfs/slac/g/glast/ground/cvs/Interleave/src/InterleaveAlg.h,v 1.1.1.1 2005/12/14 20:38:27 burnett Exp $
 
 */
 #ifndef InterleaveAlg_h
@@ -12,7 +12,8 @@ $Header$
 #include "GaudiKernel/Algorithm.h"
 #include "GaudiKernel/Property.h"
 class INTupleWriterSvc;
-
+class TTree;
+class TFile;
 
 /** @class InterleaveAlg
     @brief Gaudi Alg that will fill the ntuple with a background event
@@ -36,11 +37,26 @@ public:
     }
 
 private: 
+    void copyEventInfo(TTree* t);
+    void copyTreeData(TTree* in, TTree* out);
+
     /// access the ntupleWriter service to write out to ROOT ntuples
     INTupleWriterSvc * m_rootTupleSvc;
 
+    StringProperty m_treeName; ///< name of the tree to process
+    StringProperty m_rootFile; ///< root file to sample events from
+    int m_count;   ///< number of processed events
+    int m_downlink; ///< number of down linked events
+    int m_event; ///< current event number in root file
+
+    TFile * m_tfile;
+    TTree * m_backgroundTuple;
+    TTree * m_meritTuple;
+
+
     static double s_triggerRate;
     static double s_downlinkRate;
+
 
 };
 
