7ad1b37c868a173847658f75627557ab1c08881e
burnett <>
2006-01-08 01:56:07 +0000

    clean up documentation, style a bit

--- src/SampledBackground.cxx
+++ src/SampledBackground.cxx
@@ -2,11 +2,11 @@
 
 @brief declaration and definition of the class SampledBackground
 
-$Header: /nfs/slac/g/glast/ground/cvs/Interleave/src/SampledBackground.cxx,v 1.1.1.1 2005/12/14 20:38:27 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/Interleave/src/SampledBackground.cxx,v 1.2 2005/12/18 22:50:43 burnett Exp $
 
 */
 #include "SampledBackground.h"
-#include <cmath>
+
 #include "InterleaveAlg.h" // for static rate to use
 
 #include "flux/EventSource.h"
@@ -15,23 +15,23 @@ $Header: /nfs/slac/g/glast/ground/cvs/Interleave/src/SampledBackground.cxx,v 1.1
 SampledBackground::SampledBackground(const std::string& /*params*/)
 {}
 
-  // Gives back energy
+// Gives back energy
 double SampledBackground::energy(double /*time*/){ return 0.;}
 
-  // Gives back paticle direction in cos(theta) and phi[rad]
-   std::pair<double,double> SampledBackground::dir(double /*energy*/)
-   { 
-       return std::make_pair(0., 0.);
-   }
+// Gives back paticle direction in cos(theta) and phi[rad]
+std::pair<double,double> SampledBackground::dir(double /*energy*/)
+{ 
+    return std::make_pair(0., 0.);
+}
 
-   // Gives back the total flux into the standard area 
-   double SampledBackground::flux(double /*time*/) const // calculate the flux [c/s/m^2/sr]
-   { 
-       return  InterleaveAlg::triggerRate()/(EventSource::totalArea() * solidAngle());
-   }
+// Gives back the total flux into the standard area 
+double SampledBackground::flux(double /*time*/) const // calculate the flux [c/s/m^2/sr]
+{ 
+    return  InterleaveAlg::rate()/(EventSource::totalArea() * solidAngle());
+}
 
 
-  // Gives back solid angle from which particles come
-   double SampledBackground::solidAngle() const
-   { return 4*M_PI;}
-  
+// Gives back solid angle from which particles come
+double SampledBackground::solidAngle() const
+{ return 4*M_PI;}
+
--- src/SampledBackground.h
+++ src/SampledBackground.h
@@ -1,4 +1,6 @@
 /** @file SampledBackground.h
+    @brief declaration of SampledBackground class
+$Header$
 
 */
 #ifndef interleave_PseudoBackground_h
@@ -6,32 +8,34 @@
 
 #include "flux/Spectrum.h"
 
+/** @class SampledBackground
+    @brief  a Spectrum class that is used to implement the background sampling
+
+
+*/
 class SampledBackground: public Spectrum {
 public:
-     SampledBackground(const std::string& params);
+    SampledBackground(const std::string& params);
 
-     virtual ~SampledBackground(){};
-  
-  // Gives back energy
-  virtual double energy(double time);
+    virtual ~SampledBackground(){};
 
-  // Gives back paticle direction in cos(theta) and phi[rad]
-  virtual std::pair<double,double> dir(double energy);
+    /// Gives back energy
+    virtual double energy(double time);
 
-  // Gives back the total flux (summation of each component's flux)
-  virtual double flux(double time) const;  // calculate the flux [c/s/m^2/sr]
+    /// Gives back paticle direction in cos(theta) and phi[rad]
+    virtual std::pair<double,double> dir(double energy);
 
-  // Gives back the particle kind
-  virtual const char* particleName() const{ return "p"; }
+    /// Gives back the total flux (summation of each component's flux)
+    virtual double flux(double time) const;  // calculate the flux [c/s/m^2/sr]
 
-  // Gives back the component name
-  virtual std::string title() const{ return "SampledBackground"; }
+    /// Gives back the particle kind
+    virtual const char* particleName() const{ return "p"; }
 
-  // Gives back solid angle from which particles come
-  virtual double solidAngle() const;
-  
-  // Gives back the interval to the next event
-  //virtual double interval(double time);
+    /// Gives back the component name
+    virtual std::string title() const{ return "SampledBackground"; }
+
+    /// Gives back solid angle from which particles come
+    virtual double solidAngle() const;
 
 
 private:
