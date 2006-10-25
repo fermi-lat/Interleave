4c3f45aa7cb3b6816d2ed90cd725bd72f1486bf3
burnett <>
2006-10-25 20:57:08 +0000

    remove stuff broken by interface change

--- src/test/TestAlg.cxx
+++ src/test/TestAlg.cxx
@@ -1,7 +1,7 @@
 /** @file TestAlg.cxx
     @brief Used for test program
 
- $Header: /nfs/slac/g/glast/ground/cvs/Interleave/src/test/TestAlg.cxx,v 1.3 2006/01/13 22:13:43 burnett Exp $
+ $Header: /nfs/slac/g/glast/ground/cvs/Interleave/src/test/TestAlg.cxx,v 1.4 2006/02/20 00:48:19 burnett Exp $
 
 */
 
@@ -25,30 +25,6 @@
 
 
 #include <string>
-// copy here for testing.
-//------------------------------------------------------------------------
-double BackgroundSelection::triggerRate(double maglat)
-{
-    //y = 0.0481x3 - 0.9295x2 - 27.72x + 2082.2
-    static double c[]={2082.2, -27.72, -0.9295, 0.0481};
-
-    double x = fabs(maglat);
-    if( x< 8) x=8;
-    return  ((c[3]*x + c[2])*x +c[1])*x + c[0];
-}
-
-//------------------------------------------------------------------------
-double BackgroundSelection::downlinkRate(double  maglat )
-{
-    //y = 0.0094x3 - 0.1951x2 - 5.4218x + 508.48
-    static double c[]={508.48, -5.4218, -0.1951, 0.0094};
-
-    double x = fabs(maglat);
-    if( x< 8) x=8;
-    return ((c[3]*x + c[2])*x +c[1])*x + c[0];
-
-}
-//----
 
 /*! \class TestAlg
 \brief 
@@ -131,10 +107,7 @@ StatusCode TestAlg::initialize() {
     m_rootTupleSvc->addItem(treename, "EvtLiveTime",    &EvtLiveTime );
     m_rootTupleSvc->addItem(treename, "McSourceId",     &McSourceId );
 
-    std::cout << "Testing the rates\n\t lat     trigger     downlink\n"<< std::endl;
-    for( double lat = 0; lat<45; lat+=5){
-        std::cout << "\t" << lat << " "<< BackgroundSelection::triggerRate(lat) << "   " << BackgroundSelection::downlinkRate(lat) << std::endl;
-    }
+
     return sc;
 }
 
