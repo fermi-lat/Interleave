d87a2d530f7f0e9865c4e07fc7dc3e718620899f
jrb <>
2012-08-18 00:30:55 +0000

    support no-install headers on Windows

--- InterleaveLib.py
+++ InterleaveLib.py
@@ -1,4 +1,4 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Interleave/InterleaveLib.py,v 1.2 2008/11/10 16:04:51 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Interleave/InterleaveLib.py,v 1.3 2009/11/13 23:22:40 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['Interleave'])
@@ -10,5 +10,7 @@ def generate(env, **kw):
     env.Tool('addLibrary', library = env['gaudiLibs'])
     env.Tool('addLibrary', library = env['clhepLibs'])
     env.Tool('addLibrary', library = env['xercesLibs'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'RootIo') 
 def exists(env):
     return 1;
